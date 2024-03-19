209264046d42ae06784c787680973544c1ea62e6
unknown <matheusforsterantunes@gmail.com>
2024-03-19 19:08:36 -0300

    font errors

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -5,7 +5,7 @@ const locations = ["EAD", "Parque da Juventude", "EAD", "Jaçanã"];
 const durations = ["2022 - 2025", "2019 - 2021", "2022", "20xx - 2019"];
 const about = [
   "Graduação em Ciencia da Computação",
-  "Ensino Médio integrado ao Técnico- Informatica para Internet",
+  "Médio integrado ao Técnico - Informatica para Internet",
   "Cursos de Desenvolvimento Web",
   "Curso de Inglês com certificado de Cambridge",
 ];
@@ -16,10 +16,10 @@ const Courses = () => {
       key={index}
       className="bg-sec w-1/4 min-w-40 min-h-fit max-h-64 text-white flex flex-col gap-2 rounded-xl p-4 text-center"
     >
-      <h2 className="text-sm">{course}</h2>
-      <h2 className="text-xs">{locations[index]}</h2>
-      <h3 className="text-xs">{durations[index]}</h3>
-      <h3 className="text-sm md:text-[1.5vw] lg:text-[1.2vw] xl:text-[0.85vw]">
+      <h2 className="text-sm md:text-xl">{course}</h2>
+      <h2 className="text-xs md:text-base">{locations[index]}</h2>
+      <h3 className="text-xs md:text-sm">{durations[index]}</h3>
+      <h3 className="text-sm md:text-[1.5vw] lg:text-[1.2vw] xl:text-[1vw]">
         {about[index]}
       </h3>
     </div>
